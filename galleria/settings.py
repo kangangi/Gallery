a8d1b330dc46cc943274afcfc7fec6375c6540d2
kangangi <wanjikukangangi@gmail.com>
2020-07-16 16:21:35 +0300

    deploy to heroku

--- galleria/settings.py
+++ galleria/settings.py
@@ -14,6 +14,7 @@ import os
 import dj_database_url
 from decouple import config, Csv
 import cloudinary
+import django_heroku
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
@@ -82,6 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'galleria.urls'
@@ -159,6 +161,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
 
 cloudinary.config( 
   cloud_name =config('CLOUD_NAME'), 
